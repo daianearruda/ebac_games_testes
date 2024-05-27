1a9a088f0f502e8b9fabe6443155257fe57de65e
Daiane Arruda <daianedasilvaarruda@outlook.com>
2024-05-27 18:06:58 -0300

    testando Header

---
+++ src/components/Header/__tests__/Header.test.tsx
@@ -0,0 +1,43 @@
+import { screen } from '@testing-library/react'
+
+import Header from '..'
+
+import { renderizaComProvider } from '../../../utils/tests'
+
+describe('Testes para o componente Header', () => {
+  test('Deve renderizar corretamente', () => {
+    renderizaComProvider(<Header />)
+    expect(screen.getByText('EBAC Games')).toBeInTheDocument()
+  })
+
+  test('Deve renderizar com 2 itens no carrinho', () => {
+    renderizaComProvider(<Header />, {
+      preloadedState: {
+        carrinho: {
+          itens: [
+            {
+              id: 1,
+              categoria: 'RPG',
+              imagem: '',
+              plataformas: ['windows'],
+              preco: 150.9,
+              precoAntigo: 199.9,
+              titulo: 'Elden Ring'
+            },
+            {
+              id: 2,
+              categoria: 'RPG',
+              imagem: '',
+              plataformas: ['windows', 'PS5', 'Xbox Series'],
+              preco: 180.9,
+              precoAntigo: 250.9,
+              titulo: 'Legacy'
+            }
+          ]
+        }
+      }
+    })
+
+    expect(screen.getByTestId('qtd-carrinho').innerHTML).toContain('2 itens')
+  })
+})
